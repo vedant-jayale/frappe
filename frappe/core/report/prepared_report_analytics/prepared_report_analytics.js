53ad1fc96374db90260175e839aa01e363eb36ef
ruthra kumar <ruthra@erpnext.com>
2024-12-18 11:58:29 +0530

    feat: analytics on Prepared Reports
    (cherry picked from commit f1a60d99fe6317046e1af2ad8edf40036f0e97f1)

---
+++ frappe/core/report/prepared_report_analytics/prepared_report_analytics.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2024, Frappe Technologies and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Prepared Report Analytics"] = {
+	filters: [
+		// {
+		// 	"fieldname": "my_filter",
+		// 	"label": __("My Filter"),
+		// 	"fieldtype": "Data",
+		// 	"reqd": 1,
+		// },
+	],
+};
